d5aee5eac912eec6ea9da9a3704d3cc5b95dcc2b
Matt MacAulay <mac@twg.ca>
2016-02-18 23:29:12 -0500

    Added seed tool and blade runner quotes

--- src/learn.js
+++ src/learn.js
@@ -4,14 +4,16 @@ import db from './db'
 
 function learnTerms (terms, callback) {
   const store = db()
-  return terms.filter((term) => {
+  const filteredTerms = terms.filter((term) => {
     // Filter out terms that don't have a text component.
     // These arise when there is a contraction; the NLP library
     // creates an implicit term with an empty text field.
     return !!term.text
-  }).map((term, index) => {
+  })
+  return filteredTerms.map((term, index) => {
     const currentState = term
-    const nextState = terms[index + 1]
+    const nextState = filteredTerms[index + 1]
+
     return async.apply(store.storeState, currentState, nextState, { isStartTerm: index === 0 })
   })
 }
