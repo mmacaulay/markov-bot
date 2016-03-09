d146132905f6427b4b8804fb6c39b998d996cd09
Matt MacAulay <mac@twg.ca>
2016-03-08 22:21:01 -0500

    Filter usernames and urls

--- src/slack/learn.js
+++ src/slack/learn.js
@@ -2,12 +2,19 @@ import config from 'config'
 import request from 'request'
 
 function learn (message) {
+  // Don't learn usernames
+  const userPattern = /@[^\s]+/g
+  // Or URLs
+  const urlPattern = /http[s]?:\/\/[^\s]+/g
+
+  const text = message.text.replace(userPattern, '').replace(urlPattern, '')
+
   const req = {
     uri: `${config.slack.api}/learn`,
     json: {
       orders: config.slack.orders,
       namespaces: [message.user],
-      text: message.text
+      text: text
     }
   }
   request.post(req, (err, res) => {
