7f00b629c193da0eebfbe6b4c8fa639f24b14944
Matt MacAulay <mac@twg.ca>
2016-04-15 17:48:09 -0400

    Pass a list of hosts to ES client

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -3,7 +3,8 @@ var elasticsearch = require('elasticsearch')
 
 export function createClient () {
   return new elasticsearch.Client({
-    host: config.elasticsearch.host,
+    hosts: config.elasticsearch.hosts,
+    sniffOnStart: true,
     log: config.elasticsearch.log_level,
     apiVersion: '2.2'
   })
