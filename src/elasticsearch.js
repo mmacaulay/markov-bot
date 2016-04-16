59fa114375cce7696b5437e235f9eb6c2ef4e46c
Matt MacAulay <mac@twg.ca>
2016-04-16 13:30:00 -0400

    Fix ES hosts configuration

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -1,9 +1,15 @@
-var config = require('config')
-var elasticsearch = require('elasticsearch')
+import config from 'config'
+import elasticsearch from 'elasticsearch'
+
+// Elasticsearch hosts may be configured as an environment variable,
+// in which case it will be a string, so we parse it if necessary.
+const hosts = Array.isArray(config.elasticsearch.hosts)
+                ? config.elasticsearch.hosts
+                : JSON.parse(config.elasticsearch.hosts)
 
 export function createClient () {
   return new elasticsearch.Client({
-    hosts: config.elasticsearch.hosts,
+    hosts: hosts,
     sniffOnStart: true,
     log: config.elasticsearch.log_level,
     apiVersion: '2.2'
